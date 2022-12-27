d6f0054c38956156b01b0784c8fb55f307891442
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-12-27 15:20:33 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String text = getInputText();
-        printTextLevel(text);
+        evaluateTextLevel(text);
     }
 
-    private static boolean textIsHard(String text) {
-        return text.length() > 100;
+    private static boolean textIsHard(int averageSentenceLength) {
+        return averageSentenceLength > 10;
     }
 
     private static String getInputText() {
@@ -15,11 +15,34 @@ public class Main {
         return scanner.nextLine();
     }
 
-    private static void printTextLevel(String text) {
-        if (textIsHard(text)) {
+    private static void evaluateTextLevel(String text) {
+        String[] sentences = getSentences(text);
+        int averageLength = getAverageSentenceLength(sentences);
+
+        if (textIsHard(averageLength)) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
         }
+    }
+
+    private static String[] getSentences(String text) {
+        String[] sentences = text.split("[.?!]");
+
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].trim();
+        }
+
+        return sentences;
+    }
+
+    private static int getAverageSentenceLength(String[] sentences) {
+        double sum = 0;
+
+        for (String s : sentences) {
+            sum += s.split("\\s").length;
+        }
+
+        return (int) Math.ceil(sum / sentences.length);
     }
 }
