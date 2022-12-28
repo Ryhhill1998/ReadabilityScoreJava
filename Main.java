90630ccff45d0d0b4adb9f6723541647f96c9002
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-12-28 14:51:53 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
-//            String filePath = args[0];
             String text = getFileText("in.txt");
-            System.out.println(getWordCount(text));
-            System.out.println(getSentenceCount(text));
-            System.out.println(getCharacterCount(text));
-            System.out.println(Arrays.toString(getSentences(text)));
+            evaluateText(text);
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
@@ -23,10 +18,6 @@ public class Main {
         StringBuilder text = new StringBuilder();
 
         while (scanner.hasNextLine()) {
-            if (!text.isEmpty()) {
-                text.append(" ");
-            }
-
             text.append(scanner.nextLine());
         }
 
@@ -55,19 +46,70 @@ public class Main {
         return text.split("");
     }
 
-    private static int getSentenceCount(String text) {
+    private static double getSentenceCount(String text) {
         return getSentences(text).length;
     }
 
-    private static int getWordCount(String text) {
+    private static double getWordCount(String text) {
         return getWords(text).length;
     }
 
-    private static int getCharacterCount(String text) {
+    private static double getCharacterCount(String text) {
         return getCharacters(text).length;
     }
 
-//    private static int calculateARI() {
-//
-//    }
+    private static double calculateARI(double words, double sentences, double characters) {
+        return 4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43;
+    }
+
+    private static String getTextAgeRange(double score) {
+        String ageRange;
+
+        if (score < 2) {
+            ageRange = "5-6";
+        } else if (score < 3) {
+            ageRange = "6-7";
+        } else if (score < 4) {
+            ageRange = "7-8";
+        } else if (score < 5) {
+            ageRange = "8-9";
+        } else if (score < 6) {
+            ageRange = "9-10";
+        } else if (score < 7) {
+            ageRange = "10-11";
+        } else if (score < 8) {
+            ageRange = "11-12";
+        } else if (score < 9) {
+            ageRange = "12-13";
+        } else if (score < 10) {
+            ageRange = "13-14";
+        } else if (score < 11) {
+            ageRange = "14-15";
+        } else if (score < 12) {
+            ageRange = "15-16";
+        } else if (score < 13) {
+            ageRange = "16-17";
+        } else if (score < 14) {
+            ageRange = "17-18";
+        } else {
+            ageRange = "18-22";
+        }
+
+        return ageRange;
+    }
+
+    private static void evaluateText(String text) {
+        double words = getWordCount(text);
+        double sentences = getSentenceCount(text);
+        double characters = getCharacterCount(text);
+        double textARI = calculateARI(words, sentences, characters);
+
+        System.out.printf("Words: %.0f\n", words);
+        System.out.printf("Sentences: %.0f\n", sentences);
+        System.out.printf("Characters: %.0f\n", characters);
+        System.out.printf("The score is: %.2f\n", textARI);
+        System.out.println("This text should be understood by "
+                + getTextAgeRange(Math.ceil(textARI))
+                + " year-olds.");
+    }
 }
